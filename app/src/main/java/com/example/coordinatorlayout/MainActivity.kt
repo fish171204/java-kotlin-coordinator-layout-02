83ddfc28a8751a2cb3f7bec29324c51fab4cfda0
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-03-19 10:56:03 +0700

    Coordinator Layout

---
+++ app/src/main/java/com/example/coordinatorlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.coordinatorlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
